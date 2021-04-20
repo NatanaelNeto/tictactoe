2e7565ef09af5575eb262e76897ea4f9e1231e9c
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-04-20 16:20:25 -0300

    C1 - Adicionando logo e navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Logo from '../data/logo.png';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 
 function Header() {
+  const [menu, setMenu] = useState(false);
   return (
-    <header>
-      Teste header
+    <header className="header">
+      <div className="header_logo">
+        <img src={ Logo } alt="Tic Tac Toe" />
+        <h1>Tic Tac Toe</h1>
+      </div>
+      <div className="header_menu">
+        <button
+          className="header_menu-button"
+          type="button"
+          onClick={ () => setMenu(!menu) }
+        >
+          <MenuRoundedIcon style={ { fontSize: "50px" } } />
+        </button>
+        <nav className={ menu ? 'header_nav' : 'header_nav--disable' }>
+          <ul className="header_nav-list">
+            <li className="header_nav-element">Reiniciar</li>
+            <li className="header_nav-element">Nesn, o Game Dev</li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
